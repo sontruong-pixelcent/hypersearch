1296b72d96c5be123ef99316f8f56e5b5f04b998
QahUno <hvquan20@apcs.fitus.edu.vn>
2022-12-03 14:11:29 +0700

    about us 1.0

--- frontend/src/containers/AboutUs/index.tsx
+++ frontend/src/containers/AboutUs/index.tsx
@@ -4,8 +4,11 @@ import * as classes from "./styles.module.scss"
 
 const AboutUsContainer = memo(() => {
   return (
-    <Container>
-      <p className={classes.text}>This is about us page.</p>
+    <Container className="about-section">
+      <h2 className={classes.heading}>We are Group 10</h2>
+      <p className={classes.slogan} >Our slogan</p>
+      <p className={classes.aboutText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    
     </Container>
   )
 })
