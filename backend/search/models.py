b5dead622411f2478a3e3d5ce83f10e3ae3ab3dd
CuongP + HaiT <cuong.pham+hai.truong@eastagile.com>
2022-12-24 09:54:42 +0700

    [f] Search endpoint

--- backend/search/models.py
+++ backend/search/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Package(models.Model):
+    index = models.IntegerField(default=0)
+    fullname = models.CharField(max_length=70, default='')
+    name = models.CharField(max_length=62)
+    description = models.CharField(max_length=277, null=True)
+    gh_readme = models.TextField(null=True)
+    author = models.CharField(max_length=24)
+    stargazers_count = models.IntegerField()
+    watchers_count = models.IntegerField()
+    forks_count = models.IntegerField()
--- backend/search/views.py
+++ backend/search/views.py
@@ -1,5 +1,17 @@
+from django.core import serializers
 from django.http import JsonResponse
+from django.contrib.postgres.search import SearchQuery, SearchVector, SearchRank
+from .models import Package
 
 
 def index(request):
-    return JsonResponse({'packages': [{'id': 'package id', 'name': 'package name'}]})
+    query = request.GET.get("q", "")
+
+    search_rank = SearchRank(SearchVector(
+        "fullname", "gh_readme", "author", "description"), SearchQuery(query))
+
+    results = serializers.serialize("json", Package.objects.annotate(
+        rank=search_rank
+    ).order_by("-rank")[:5])
+
+    return JsonResponse({'packages': results})
